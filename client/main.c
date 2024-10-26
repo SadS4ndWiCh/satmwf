8dec2bfeb6bcd3e1031289c5c1aa813252edc66b
sads4ndwich <caiocamargo007@gmail.com>
2024-10-26 10:42:35 -0300

    feat: client event loop

--- client/main.c
+++ client/main.c
@@ -1,74 +1,60 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/epoll.h>
 #include <netinet/in.h>
 
 #include "sock.h"
+#include "client.h"
 #include "protocol.h"
 
-int main(void) {
-    int server_fd = setup_sock(INADDR_LOOPBACK, 3000);
-
-    struct CONMessage con = { "Caio" };
-    struct Message msg = {
-        .length  = sizeof(con),
-        .type    = MCON,
-        .payload = (u8 *) &con
+int main(int argc, char **argv) {
+    struct Client client = {
+        .nick = "guest1280",
+        .host = INADDR_LOOPBACK,
+        .port = 3000
     };
 
-    if (Message_send(server_fd, &msg)) {
-        printf("%s:%d ERROR: failed to send request connection message to server.\n", __FILE__, __LINE__);
+    if (argc == 2) {
+        strcpy(client.nick, argv[1]);
+    }
+
+    if (Client_init(&client) == -1) {
         return 1;
     }
 
-    printf("%s:%d INFO: request to connect the server: CON\n", __FILE__, __LINE__);
+    struct epoll_event input_ev;
+    input_ev.events = EPOLLIN;
+    input_ev.data.fd = STDIN_FILENO;
 
-    struct Message con_result;
-    if (Message_recv(server_fd, &con_result) == -1) {
-        printf("%s:%d ERROR: failed to receive request connection reply.\n", __FILE__, __LINE__);
-        return 1;
+    if (epoll_ctl(client.pollfd, EPOLL_CTL_ADD, STDIN_FILENO, &input_ev) == -1) {
+        fprintf(stderr, "%s:%d ERROR: fail to add input event to poll.\n", __FILE__, __LINE__);
+        return 2;
     }
 
-    if (con_result.type == MFCN) {
-        printf("%s:%d ERROR: failed to connect to chat room.\n", __FILE__, __LINE__);
-        return 1;
+    if (Client_join_chat(&client) == -1) {
+        return 3;
     }
 
-    if (con_result.type != MSCN) {
-        printf("%s:%d ERROR: receive an unexpected message: %#02x\n", __FILE__, __LINE__, con_result.type);
-        return 1;
+    struct epoll_event events[SOCK_QUEUE_MAX];
+    while (1) {
+        int nfds = epoll_wait(client.pollfd, events, SOCK_QUEUE_MAX, -1);
+        if (nfds == -1) {
+            fprintf(stderr, "%s:%d ERROR: fail to get ready events.\n", __FILE__, __LINE__);
+            continue;
+        }
+
+        for (int i = 0; i < nfds; i++) {
+            struct epoll_event ev = events[i];
+            if (!(ev.events & EPOLLIN)) continue;
+
+            if (ev.data.fd == client.fd) {
+                Client_handle_message(&client);
+            } else if (ev.data.fd == STDIN_FILENO) {
+                Client_send_message(&client);
+            }
+        }
     }
-
-    u8 userid = ((struct SCNMessage *) con_result.payload)->id;
-    printf("%s:%d INFO: successfuly joined the receives and receives the id: %d\n", __FILE__, __LINE__, userid);
-
-    struct MSGMessage chat_message = {
-        .author_id = userid,
-        .message   = "Hello World!",
-    };
-
-    msg.length  = sizeof(chat_message);
-    msg.type    = MMSG;
-    msg.payload = (u8 *) &chat_message;
-
-    if (Message_send(server_fd, &msg) == -1) {
-        printf("ERROR: fail to send chat message.\n");
-        return 1;
-    }
-
-    printf("%s:%d INFO: send chat message to server: '%s'\n", __FILE__, __LINE__, chat_message.message);
-
-    msg.length  = 0;
-    msg.type    = MDIS;
-    msg.payload = NULL;
-
-    if (Message_send(server_fd, &msg) == -1) {
-        printf("ERROR: fail to request to disconnect. closing anyway...\n");
-        close(server_fd);
-    }
-
-    printf("%s:%d INFO: request to disconnect the server: DIS\n", __FILE__, __LINE__);
-
-    printf("INFO: disconnected from server.\n");
 
     return 0;
 }
